78fa246d372719a10b925ddf92a1139e60e380d8
luisrossim <luisrossim23@gmail.com>
2023-04-18 03:39:09 -0300

    post mapping/response body cliente

--- src/main/java/com/luisn/crudspring/controller/ClienteController.java
+++ src/main/java/com/luisn/crudspring/controller/ClienteController.java
@@ -3,8 +3,12 @@ package com.luisn.crudspring.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,16 +17,23 @@ import com.luisn.crudspring.repository.ClienteRepository;
 
 import lombok.AllArgsConstructor;
 
+
 @RestController
 @RequestMapping("/api/clientes")
 @AllArgsConstructor
 public class ClienteController {
-    
-    private final ClienteRepository clienteRepository;
 
+    private final ClienteRepository clienteRepository;
 
     @GetMapping
     public List<Cliente> listar(){
         return clienteRepository.findAll();
+    }
+
+    @PostMapping
+    public ResponseEntity<Cliente> create(@RequestBody Cliente registrocliente){
+        //System.out.println(registrocliente.getName());
+        //return clienteRepository.save(registrocliente);
+        return ResponseEntity.status(HttpStatus.CREATED).body(clienteRepository.save(registrocliente));
     }
 }
--- src/main/java/com/luisn/crudspring/model/Cliente.java
+++ src/main/java/com/luisn/crudspring/model/Cliente.java
@@ -1,5 +1,7 @@
 package com.luisn.crudspring.model;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
