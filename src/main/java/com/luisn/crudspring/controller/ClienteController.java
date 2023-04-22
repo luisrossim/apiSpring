bba1eea3fced82bb40e34097c681373e187ce280
luisrossim <luisrossim23@gmail.com>
2023-04-22 19:24:33 -0300

    add getbyid

--- src/main/java/com/luisn/crudspring/controller/ClienteController.java
+++ src/main/java/com/luisn/crudspring/controller/ClienteController.java
@@ -7,6 +7,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -28,6 +29,13 @@ public class ClienteController {
     @GetMapping
     public List<Cliente> listar(){
         return clienteRepository.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Cliente> buscarPorID(@PathVariable long id){
+        return clienteRepository.findById(id)
+            .map(registro -> ResponseEntity.ok().body(registro))
+            .orElse(ResponseEntity.notFound().build());
     }
 
     @PostMapping
