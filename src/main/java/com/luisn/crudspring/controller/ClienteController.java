c977c4de97c31fc292e812a3697f4c6cce2e48ef
luisrossim <luisrossim23@gmail.com>
2023-04-22 23:33:29 -0300

    add post update

--- src/main/java/com/luisn/crudspring/controller/ClienteController.java
+++ src/main/java/com/luisn/crudspring/controller/ClienteController.java
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -27,21 +28,36 @@ public class ClienteController {
     private final ClienteRepository clienteRepository;
 
     @GetMapping
-    public List<Cliente> listar(){
+    public List<Cliente> listar() {
         return clienteRepository.findAll();
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<Cliente> buscarPorID(@PathVariable long id){
+    public ResponseEntity<Cliente> buscarPorID(@PathVariable long id) {
         return clienteRepository.findById(id)
             .map(registro -> ResponseEntity.ok().body(registro))
             .orElse(ResponseEntity.notFound().build());
     }
 
     @PostMapping
-    public ResponseEntity<Cliente> create(@RequestBody Cliente registrocliente){
+    public ResponseEntity<Cliente> create(@RequestBody Cliente registrocliente) {
         //System.out.println(registrocliente.getName());
         //return clienteRepository.save(registrocliente);
         return ResponseEntity.status(HttpStatus.CREATED).body(clienteRepository.save(registrocliente));
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Cliente> update(@PathVariable long id, @RequestBody Cliente registrocliente) {
+
+        return clienteRepository.findById(id)
+            .map(registrobusca -> {
+                registrobusca.setName(registrocliente.getName());
+                registrobusca.setIdade(registrocliente.getIdade());
+                registrobusca.setCidade(registrocliente.getCidade());
+                registrobusca.setCategoria(registrocliente.getCategoria());
+                Cliente atualizado = clienteRepository.save(registrobusca);
+                return ResponseEntity.ok().body(atualizado);
+            })
+            .orElse(ResponseEntity.notFound().build());
     }
 }
